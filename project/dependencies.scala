ffe588cb5339a983857761b125021e0cb29a8652
kristian <kristian@rememberthemilk.com>
2013-04-08 16:52:03 +1000

    Bump rabbitmq amqp-client and akka versions

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object dependencies {
   def Scalatest    = "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP5" % "test"   	  // ApacheV2
   def scalaActorsForScalaTest = "org.scala-lang" % "scala-actors" % "2.10.0" % "test"
-  def AmqpClient = "com.rabbitmq" % "amqp-client" % "3.0.2"   													  // ApacheV2
+  def AmqpClient = "com.rabbitmq" % "amqp-client" % "3.0.4"   													  // ApacheV2
 
  
-  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.1.1"
+  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.1.2"
 
   def Specs2      = "org.specs2"                 % "specs2_2.10"              % "1.13"        % "test"  // MIT
   def JUnit = "junit" % "junit" % "4.7" % "test"   																 // Common Public License 1.0
-  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.10" % "2.1.1" % "test"
+  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.10" % "2.1.2" % "test"
   //def ActorTests = "com.typesafe.akka" % "akka-actor-tests_2.10.0-M7" % "2.1-M2" % "test"
   def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"     											// MIT
 }
