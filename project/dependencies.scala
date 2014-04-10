ac499667ef1079e05b4a56eb7707a90043c03523
kristian <kristian@rememberthemilk.com>
2014-04-10 10:27:11 +1000

    Bump akka

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,7 +6,7 @@ object dependencies {
   def AmqpClient = "com.rabbitmq" % "amqp-client" % "3.3.0"                               // ApacheV2
 
 
-  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.3.1"
+  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.3.2"
 
   def Specs2      = "org.specs2"                 % "specs2_2.10"              % "1.13"        % "test"  // MIT
   def JUnit = "junit" % "junit" % "4.7" % "test"                                   // Common Public License 1.0
