f6ab3895b90961ad1ef8f057425d68cac08a0f80
kristian <kristian@rememberthemilk.com>
2014-04-25 12:50:00 +1000

    Bump scala, sbt, plugins

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object dependencies {
-  def Scalatest    = "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP5" % "test"       // ApacheV2
-  def scalaActorsForScalaTest = "org.scala-lang" % "scala-actors" % "2.10.4" % "test"
+  def Scalatest    = "org.scalatest" % "scalatest_2.11" % "2.1.3" % "test"       // ApacheV2
+  def scalaActorsForScalaTest = "org.scala-lang" % "scala-actors" % "2.11.0" % "test"
   def AmqpClient = "com.rabbitmq" % "amqp-client" % "3.3.0"                               // ApacheV2
 
 
-  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.3.2"
+  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.11" % "2.3.2"
 
-  def Specs2      = "org.specs2"                 % "specs2_2.10"              % "1.13"        % "test"  // MIT
+  def Specs2      = "org.specs2"                 % "specs2_2.11"              % "2.3.11"        % "test"  // MIT
   def JUnit = "junit" % "junit" % "4.7" % "test"                                   // Common Public License 1.0
-  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.10" % "2.2.1" % "test"
+  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.11" % "2.3.2" % "test"
   //def ActorTests = "com.typesafe.akka" % "akka-actor-tests_2.10.0-M7" % "2.1-M2" % "test"
   def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"                          // MIT
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,9 +2,8 @@
 
 resolvers += Classpaths.typesafeResolver
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.0.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.3")
-
+addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
