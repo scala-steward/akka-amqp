6e4d1984143d36e4e9458bde3ddb3713b51d4a95
omar <omar@rememberthemilk.com>
2017-11-17 16:37:14 -0800

    Update scala to 2.12.4, RabbitMQ to 5.0.0, Akka to 2.5.6.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object dependencies {
-  def AmqpClient = "com.rabbitmq" % "amqp-client" % "4.1.0"
-  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.4.17"
+  def AmqpClient = "com.rabbitmq" % "amqp-client" % "5.0.0"
+  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.5.6"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,4 +8,4 @@ addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "3.0.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
 
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.1")
