e677e42866e94b79ce7112db2afc00ec7ce5c87e
omar <omar@rememberthemilk.com>
2019-04-09 21:28:33 -0700

    Update deps.

--- project/AkkaAmqpBuild.scala
+++ project/AkkaAmqpBuild.scala
@@ -24,7 +24,7 @@ object AkkaAmqpBuild extends Build {
     resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/releases/",
     organization := "com.github.cessationoftime",
     version      := "2.5-SNAPSHOT",
-    scalaVersion := "2.12.4"
+    scalaVersion := "2.12.8"
   )
 
   lazy val root = Project(
--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object dependencies {
-  def AmqpClient = "com.rabbitmq" % "amqp-client" % "5.0.0"
-  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.5.6"
+  def AmqpClient = "com.rabbitmq" % "amqp-client" % "5.7.0"
+  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.5.22"
 }
